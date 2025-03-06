bee35f0c1e8be7d2432257f852e6aba5729b0136
SmithFerreira-07 <GitFerreira@proton.me>
2025-03-06 19:40:10 -0300

    nav and footer

---
+++ scripts/getDates.js
@@ -0,0 +1,4 @@
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+const lastModification = document.lastModified;
+document.getElementById("modification").textContent = lastModification;
