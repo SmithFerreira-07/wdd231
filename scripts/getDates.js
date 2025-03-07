df98bd67854ab8acc0b8b15aaf24a9bb160a0d77
SmithFerreira-07 <GitFerreira@proton.me>
2025-03-07 14:51:40 -0300

    added alternate img and media query

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,3 +2,13 @@ const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 const lastModification = document.lastModified;
 document.getElementById("modification").textContent = lastModification;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menuButton');
+    const mainNav = document.getElementById('homeNav');
+    
+    menuButton.addEventListener('click', function() {
+        mainNav.classList.toggle('active');
+    });
+});
