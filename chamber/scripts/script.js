a7b2d3e2c12339b2f35e665bc929535e01109036
SmithFerreira-07 <GitFerreira@proton.me>
2025-03-14 23:24:57 -0300

    fetch function

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,2 +1,18 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const membersContainer = document.getElementById("members");
+    const toggleButton = document.getElementById("toggleView");
+
+    async function fetchMembers() {
+        try {
+            const response = await fetch("data/members.json");
+            const members = await response.json();
+            displayMembers(members);
+        } catch (error) {
+            console.error("Error fetching json members:", error);
+        }
+    }
+
+    
