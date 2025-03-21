ab25d7da14b8b3bc74eb5719adc731d687c9c2bf
SmithFerreira-07 <GitFerreira@proton.me>
2025-03-21 19:43:32 -0300

    making async fetch function for weather api

--- chamber/scripts/scripts_home.js
+++ chamber/scripts/scripts_home.js
@@ -3,6 +3,26 @@ document.getElementById("lastModified").textContent = document.lastModified;
 document.addEventListener("DOMContentLoaded", () => {
     const hamburgerButton = document.getElementById("mobileMenu");
     const mainNav = document.getElementById("navigation");
+    const weatherContainer = document.getElementById("weatherNow");
+    const API_KEY = "af1fb6099766ced58ea106b357b8fd94";
+    const LATITUDE = 16.7666;
+    const LONGITUDE = -3.0026;
+
+    async function getWeatherData() {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${LATITUDE}&lon=${LONGITUDE}&units=metric&appid=${API_KEY}`);
+            
+            if (!response.ok) {
+                throw new Error("Weather data not available");
+            }
+            
+            const data = await response.json();
+            displayWeather(data);
+        } catch (error) {
+            console.error("Error fetching weather data:", error);
+            weatherContainer.innerHTML = "<p>Weather information unavailable</p>";
+        }
+    }
 
 
     hamburgerButton.addEventListener("click", () => {
